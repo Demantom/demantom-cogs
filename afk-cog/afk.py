925dcafb5c9b8e4cc7139c8e29947c2ee28420d3
Alex <sashazemskov@gmail.com>
2023-10-12 14:27:41 +0300

    first test if works

--- afk-cog/afk.py
+++ afk-cog/afk.py
@@ -1,4 +1,5 @@
 from typing import Dict, Optional, Set
+from datetime import datetime
 import discord
 from redbot import VersionInfo, version_info
 from redbot.core import Config, checks, commands
@@ -6,6 +7,7 @@ from redbot.core.bot import Red
 
 
 class Afk(commands.cog):
+    "Afk cog which will make programmers break their tables and start drinking"
     __version__ = "0.0.1"
     __author__ = ["Demantom"]
 
@@ -13,3 +15,34 @@ class Afk(commands.cog):
         self.bot: Red = bot
         default_guild: Dict[str, Optional[int]] = {"mute_role": None}
         self.config.register_guild(**default_guild)
+        self.config.register_guild(**self.default_guild_settings)
+        self.config.register_user(**self.default_user_settings)
+
+    async def afk_settings(self, ctx):
+        author = ctx.author
+        msg = ""
+        data = {
+            "MESSAGE": "user is afk"
+
+        }
+
+    @commands.command(name="afk")
+    async def afk_(self, ctx, delete_after: Optional[int] = None, *, message: str = None):
+        """
+            sets afk
+        """
+        if delete_after is not None and delete_after < 5:
+            return await ctx.send("Please set a time longer than 5 seconds for the `delete_after` argument")
+
+        author = ctx.message.author
+        mess = await self.config.user(author).MESSAGE()
+        if mess:
+            await self.config.user(author).MESSAGE.set(False)
+            msg = "You're now back."
+        else:
+            if message is None:
+                await self.config.user(author).MESSAGE.set((" ", delete_after))
+            else:
+                await self.config.user(author).MESSAGE.set((message, delete_after))
+            msg = "You're now set as afk."
+        await ctx.send(msg)
